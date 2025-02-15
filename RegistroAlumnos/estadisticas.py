5650eb278459d521d7d44faf65db4fd652edd3bb
Wilmer-abc <wbatzc@miumg.edu.gt>
2025-02-14 20:03:11 -0600

    obtine a los alumnos inscritos por edad

--- RegistroAlumnos/estadisticas.py
+++ RegistroAlumnos/estadisticas.py
@@ -4,7 +4,12 @@ def obtener_estadisticas_edad():
     conexion = conectar_bd()
     if conexion:
         cursor = conexion.cursor()
-        sql = "SELECT YEAR(CURDATE()) - YEAR(FechaNacimiento) AS edad, COUNT(*) FROM alumnos GROUP BY edad"
+        sql = """
+            SELECT TIMESTAMPDIFF(YEAR, FechaNacimiento, CURDATE()) AS edad, COUNT(*) 
+            FROM alumnos 
+            GROUP BY edad 
+            ORDER BY edad;
+        """
         cursor.execute(sql)
         estadisticas = cursor.fetchall()
         conexion.close()
